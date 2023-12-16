042884fbe809721c4e0f2e6eaf29b7b8b2093930
Manika Agarwal <77720999+manika-6303@users.noreply.github.com>
2023-12-16 21:52:29 +0530

    Update main.go

--- main.go
+++ main.go
@@ -2,20 +2,43 @@ package main
 
 import "gofr.dev/pkg/gofr"
 
-
-func initializeRouter(){
-
-	app:=gofr.New()
-
-	app.GET("/employees", GetEmp)
-	app.GET("/employees/{id}", GetEmp)
-	app.POST("/employees", PostEmp)
-	app.PUT("employees/{id}", PutEmp)
-	app.DELETE("/employees/{id}", DeleteEmp)
-
-	log.fatal(http.ListenAndServe(":9000",app))
+type Customer struct {
+    ID   int    `json:"id"`
+    Name string `json:"name"`
 }
+func post(ctx *gofr.Context) (interface{}, error){
+    name := ctx.PathParam("name")
 
-func main(){
-	initializeRouter()
+        // Inserting a customer row in database using SQL
+        _, err := ctx.DB().ExecContext(ctx, "INSERT INTO customers (name) VALUES (?)", name)
+
+        return nil, err
+}
+func get(ctx *gofr.Context) (interface{}, error){
+    var customers []Customer
+
+        // Getting the customer from the database using SQL
+        rows, err := ctx.DB().QueryContext(ctx, "SELECT * FROM customers")
+        if err != nil {
+            return nil, err
+        }
+
+        for rows.Next() {
+            var customer Customer
+            if err := rows.Scan(&customer.ID, &customer.Name); err != nil {
+                return nil, err
+            }
+
+            customers = append(customers, customer)
+        }
+
+        // return the customer
+        return customers, nil
+}
+func main() {
+    // initialise gofr object
+    app := gofr.New()
+
+    app.POST("/customer/{name}",post)
+    app.Start()
 }
